068af2f1ab6a1e23fef897556aa2655dc9fe7e24
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 07:07:44 -0500

    name before url appparently

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "CandleTracing", targets: ["CandleTracing"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/candlefinance/swift-service-context.git", name: "candle-swift-service-context", branch: "fix-candle-1.2.1")
+        .package(name: "candle-swift-service-context", url: "https://github.com/candlefinance/swift-service-context.git", branch: "fix-candle-1.2.1")
     ],
     targets: [
         // ==== --------------------------------------------------------------------------------------------------------
