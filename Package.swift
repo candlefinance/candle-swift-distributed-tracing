0892cc20bafbb3bf321dd8d98847dc99ebdce122
Nikolai Wotton <wottonn@gmail.com>
2026-01-22 05:52:36 -0500

    prefixing candle- to package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-distributed-tracing",
+    name: "candle-swift-distributed-tracing",
     products: [
         .library(name: "CandleInstrumentation", targets: ["CandleInstrumentation"]),
         .library(name: "CandleTracing", targets: ["CandleTracing"]),
